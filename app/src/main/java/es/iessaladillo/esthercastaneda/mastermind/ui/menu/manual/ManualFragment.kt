b1648fe600f5e003a78f163a60215288ea99f73d
Esther <esthercmprogramacion@gmail.com>
2020-05-14 07:31:43 +0200

    Manual completado

--- app/src/main/java/es/iessaladillo/esthercastaneda/mastermind/ui/menu/manual/ManualFragment.kt
+++ app/src/main/java/es/iessaladillo/esthercastaneda/mastermind/ui/menu/manual/ManualFragment.kt
@@ -26,42 +26,31 @@ class ManualFragment : Fragment(R.layout.fragment_manual) {
     private fun setupViews() {
         setupAdapter()
         setupViewPager()
+        setupTabLayout()
         btnExit.setOnClickListener { navController.navigateUp() }
     }
 
     private fun setupAdapter() {
         val pages = listOf(
+            Page(R.drawable.manual_howttoplay_es, R.drawable.manual_howttoplay_en),
+            Page(R.drawable.manual_emptychip_es, R.drawable.manual_emptychip_en),
+            Page(R.drawable.manual_whitechip_es, R.drawable.manual_whitechip_en),
+            Page(R.drawable.manual_blackchip_es, R.drawable.manual_blackchip_en),
+            Page(R.drawable.manual_modegame_es, R.drawable.manual_modegame_en),
             Page(R.drawable.manual_singleplayer_es, R.drawable.manual_singleplayer_en),
-            Page(R.drawable.manual_multiplayer_es, R.drawable.manual_multiplayer_en)
+            Page(R.drawable.manual_multiplayer_es, R.drawable.manual_multiplayer_en),
+            Page(R.drawable.manual_difficulty_es, R.drawable.manual_difficulty_en),
+            Page(R.drawable.manual_easy_es, R.drawable.manual_easy_en),
+            Page(R.drawable.manual_medium_es, R.drawable.manual_medium_en),
+            Page(R.drawable.manual_hard_es, R.drawable.manual_hard_en)
         )
-            /*Page(R.drawable.ic_videogame_asset_black_24dp,
-                "Fácil:\n" +
-                        "\to Nº Fichas: 4\n" +
-                        "\to Nº Rondas: 10\n" +
-                        "\to ¿Se repite los colores en la combinación secreta?: No\n"),
-            Page(R.drawable.ic_videogame_asset_black_24dp, "Medio:\n" +
-                        "\to Nº Fichas: 5\n" +
-                        "\to Nº Rondas: 15\n" +
-                        "\to ¿Se repite los colores en la combinación secreta?: Sí\n"),
-            Page(R.drawable.ic_videogame_asset_black_24dp, "Difícil:\n" +
-                        "\to Nº Fichas: 6\n" +
-                        "\to Nº Rondas: 20\n" +
-                        "\to ¿Se repite los colores en la combinación secreta?: Sí"),
-            Page(R.drawable.ic_videogame_asset_black_24dp,
-                "Un jugador: Tendrás que adivinar la combinación secreta antes de llegar al límite de rondas."),
-            Page(R.drawable.ic_videogame_asset_black_24dp,
-                "Multijugador: Juegas contra la IA, tendrás que adivinar la combinación secreta antes que la IA o antes de llegar al límite de rondas."),
-            Page(R.drawable.manual_chip_empty,
-                "Ficha vacía: Hay una ficha que no aparece en la combinación secreta."),
-            Page(R.drawable.manual_chip_white,
-                "Ficha blanca: Hay una ficha que aparece en la combinación secreta, pero no está en la misma posición a la secreta."),
-            Page(R.drawable.manual_chip_black,
-                "Ficha negra: Hay una ficha que aparece en la combinación secreta y está en la misma posición.")
-        )*/
         manualAdapter = ManualFragmentAdapter(pages).also {
             it.onItemClickListener = { navController.navigateUp() }
         }
     }
 
     private fun setupViewPager() { viewPager.adapter = manualAdapter }
+    private fun setupTabLayout() {
+        TabLayoutMediator(tabLayout, viewPager) { _, _ -> }.attach()
+    }
 }
