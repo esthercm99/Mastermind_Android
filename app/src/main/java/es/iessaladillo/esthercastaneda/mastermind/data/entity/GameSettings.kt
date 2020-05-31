0bc71649b5392e20c78b83595545fc1a696ba415
Esther <esthercmprogramacion@gmail.com>
2020-05-31 07:39:57 +0200

    Correccion de un despiste en niveles

--- app/src/main/java/es/iessaladillo/esthercastaneda/mastermind/data/entity/GameSettings.kt
+++ app/src/main/java/es/iessaladillo/esthercastaneda/mastermind/data/entity/GameSettings.kt
@@ -2,6 +2,6 @@ package es.iessaladillo.esthercastaneda.mastermind.data.entity
 
 enum class GameSettings(val numChips: Int, val numRounds: Int, val repeatColor: Boolean) {
     EASY(4, 10, false),
-    MEDIUM(5, 10, true),
+    MEDIUM(5, 15, true),
     HARD(6,20, true)
 }
